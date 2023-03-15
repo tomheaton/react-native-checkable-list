75ad3746ef2fb33dabd3317fa5e0ef86a0671588
Tom Heaton <thomas.m.heaton@gmail.com>
2023-03-15 20:12:57 +0000

    feat: test new import statement

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import CheckableList from './CheckableList';
 import useCheckedItems from './useCheckedItems';
 
+// export { useCheckedItems } from './useCheckedItems';
 export { useCheckedItems };
 
 export default CheckableList;
